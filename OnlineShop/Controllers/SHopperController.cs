f65f5e8ed6a616843180722ebd1b50affe780d2c
Ognjen <ognjen.sekulic1408@gmail.com>
2023-09-07 14:35:37 +0200

    fixes

--- OnlineShop/Controllers/SHopperController.cs
+++ OnlineShop/Controllers/SHopperController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ServiceLayer;
+using ServiceLayer.DataBase.Auth;
+using ServiceLayer.DataBase.Order;
 using ServiceLayer.Services.ServiceInterfaces;
 using System;
 using System.Collections.Generic;
@@ -21,13 +23,134 @@ namespace OnlineShop.Controllers
             this.shopperService = shopperService;
         }
 
+
 		[HttpGet("articles")]
-		[Authorize(Roles = "Customer")]
+		[Authorize(Roles = "Shopper")]
 		public IActionResult GetAllArticles()
 		{
 			try
 			{
 				IServiceOperationResult operationResult = shopperService.GetAllArticles();
+
+				if (!operationResult.IsSuccessful)
+				{
+					return StatusCode((int)operationResult.ErrorCode, operationResult.ErrorMessage);
+				}
+
+				return Ok(operationResult.Dto);
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError);
+			}
+		}
+
+		[HttpGet("finished-orders")]
+		[Authorize(Roles = "Shopper")]
+		public IActionResult GetFinishedOrders()
+		{
+			try
+			{
+				string token = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").LastOrDefault();
+				JwtDto jwtDto = new JwtDto(token);
+
+				IServiceOperationResult operationResult = shopperService.GetFinishedOrders(jwtDto);
+
+				if (!operationResult.IsSuccessful)
+				{
+					return StatusCode((int)operationResult.ErrorCode, operationResult.ErrorMessage);
+				}
+
+				return Ok(operationResult.Dto);
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError);
+			}
+		}
+
+		[HttpGet("pending-orders")]
+		[Authorize(Roles = "Shopper")]
+		public IActionResult GetPendingOrders()
+		{
+			try
+			{
+				string token = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").LastOrDefault();
+				JwtDto jwtDto = new JwtDto(token);
+
+				IServiceOperationResult operationResult = shopperService.GetPendingOrders(jwtDto);
+
+				if (!operationResult.IsSuccessful)
+				{
+					return StatusCode((int)operationResult.ErrorCode, operationResult.ErrorMessage);
+				}
+
+				return Ok(operationResult.Dto);
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError);
+			}
+		}
+
+		[HttpPost("order")]
+		[Authorize(Roles = "Shopper")]
+		public IActionResult PlaceOrder(PlacedOrderDto orderDto)
+		{
+			try
+			{
+				string token = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").LastOrDefault();
+				JwtDto jwtDto = new JwtDto(token);
+
+				IServiceOperationResult operationResult = shopperService.PlaceOrder(orderDto, jwtDto);
+
+				if (!operationResult.IsSuccessful)
+				{
+					return StatusCode((int)operationResult.ErrorCode, operationResult.ErrorMessage);
+				}
+
+				return Ok();
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError);
+			}
+		}
+
+		[HttpDelete("order")]
+		[Authorize(Roles = "Shopper")]
+		public IActionResult CancelOrder([FromQuery] long orderId)
+		{
+			try
+			{
+				string token = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").LastOrDefault();
+				JwtDto jwtDto = new JwtDto(token);
+
+				IServiceOperationResult operationResult = shopperService.CancelOrder(orderId, jwtDto);
+
+				if (!operationResult.IsSuccessful)
+				{
+					return StatusCode((int)operationResult.ErrorCode, operationResult.ErrorMessage);
+				}
+
+				return Ok();
+			}
+			catch (Exception)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError);
+			}
+		}
+
+		[HttpGet("order")]
+		[Authorize(Roles = "Shopper")]
+		public IActionResult GetOrderDetails([FromQuery] long id)
+		{
+			try
+			{
+				string token = Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").LastOrDefault();
+				JwtDto jwtDto = new JwtDto(token);
+
+				IServiceOperationResult operationResult = shopperService.GetOrderInfo(id);
 
 				if (!operationResult.IsSuccessful)
 				{
--- ServiceLayer/Services/ServiceInterfaces/IShopperService.cs
+++ ServiceLayer/Services/ServiceInterfaces/IShopperService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ServiceLayer.DataBase.Auth;
+using ServiceLayer.DataBase.Order;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +9,17 @@ using System.Threading.Tasks;
 namespace ServiceLayer.Services.ServiceInterfaces
 {
     public interface IShopperService
-    {
-        public IServiceOperationResult GetAllArticles();
-    }
+	{
+		IServiceOperationResult GetAllArticles();
+
+		IServiceOperationResult GetPendingOrders(JwtDto jwtDto);
+
+		IServiceOperationResult GetFinishedOrders(JwtDto jwtDto);
+
+		IServiceOperationResult PlaceOrder(PlacedOrderDto orderDto, JwtDto jwDto);
+
+		IServiceOperationResult CancelOrder(long orderId, JwtDto jwtDto);
+
+		IServiceOperationResult GetOrderInfo(long id);
+	}
 }
